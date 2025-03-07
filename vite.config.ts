f81cc2c1d65a33293cdf2218919dea09a4eaa87f
Artem Kuznetsov <kuznetsov_artemiy@icloud.com>
2025-03-07 03:36:50 +0300

    add server host to vite allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ export default defineConfig( {
   plugins: [react()],
   server: {
     port: 5173,
-    host: true
+    host: true,
+    allowedHosts: [
+      'localhost',
+      'artemkuznecov.ru'
+    ]
   }
 } )
