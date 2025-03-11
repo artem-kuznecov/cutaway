5f79e992bdeec2a345a2fca484545aef577325d3
Artem Kuznetsov <kuznetsov_artemiy@icloud.com>
2025-03-11 15:42:00 +0300

    add header shivering on mouse move

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, MouseEvent } from 'react'
 import cn from 'classnames'
 import animojiVideo from '@/assets/animoji.mp4'
 import styles from './Header.module.scss'
@@ -6,7 +6,7 @@ import styles from './Header.module.scss'
 type TypeCaretPosition = 'left' | 'right'
 type TypeCurrentLanguage = 'ru' | 'en'
 
-export const Header = () => {
+export const Header = (): React.JSX.Element => {
   const animojiRef = useRef<HTMLVideoElement>(null)
 
   const [caretPosition, setCaretPosition] = useState<TypeCaretPosition>('left')
@@ -22,31 +22,81 @@ export const Header = () => {
     animoji.play()
   }
 
+  function switchCaretPosition () {
+    if (caretPosition === 'left') setCaretPosition('right')
+    else setCaretPosition('left')
+  }
+
+  function mouseMoveInHeader (e: MouseEvent<HTMLDivElement>) {
+    e.stopPropagation()
+    const targetNode = e.target as HTMLDivElement
+    const position = {
+      x: e.clientX,
+      y: e.clientY,
+      blockCenterX: targetNode.getBoundingClientRect().right + targetNode.getBoundingClientRect().width / 2
+    }
+
+    let xPerc = 0
+    let yPerc = 0
+
+    // Если блок находится примерно в середине
+    if (position.x - window.innerWidth / 2 < 250) {
+      xPerc = (position.x - window.innerWidth / 2) / 40
+      yPerc = position.y / 40
+    }
+    // Если блок находится правее середины
+    else if (position.x > window.innerWidth / 2 - 250) {
+      xPerc = (position.x - position.blockCenterX) / 30
+      yPerc = position.y / 30
+    }
+    // Если блок находится левее середины
+    else {
+      xPerc = (position.blockCenterX - position.x) / 30
+      yPerc = position.y / 30
+    }
+
+    targetNode.style.transform = `translate3d(${xPerc}px, ${yPerc}px, 0px)`
+  }
+
+  function mouseOutOfBlock (e: MouseEvent<HTMLDivElement>) {
+    const targetNode = e.target as HTMLDivElement
+    targetNode.style.transform = 'none'
+  }
+
   return (
     <header className={styles.header}>
-      <div className={styles.animoji}>
+      <div
+        className={styles.animoji}
+        onMouseEnter={animojiMouseEnter}
+      >
         <video
           ref={animojiRef}
           src={animojiVideo}
-          onMouseEnter={animojiMouseEnter}
-        ></video>
+          muted={true}
+        />
       </div>
-      <div className={styles['topic-toggle']}>
+      <div
+        className={styles['topic-toggle']}
+        onClick={switchCaretPosition}
+        onMouseMove={e => mouseMoveInHeader(e)}
+        onMouseOut={mouseOutOfBlock}
+      >
         <h4
           className={cn(['text-header-4', {
             'text-gradient': caretPosition === 'left'
-          }])}
-          onClick={() => setCaretPosition('left')}
-        >Работа</h4>
+          }])}>Работа</h4>
         <h4
           className={cn(['text-header-4', {
             'text-gradient-colored': caretPosition === 'right'
-          }])}
-          onClick={() => setCaretPosition('right')}
-        >О себе</h4>
+          }])}>О себе</h4>
         <div data-role='caret' data-position={caretPosition}></div>
       </div>
-      <div className={styles['lang-toggle']} onClick={switchLanguage}>
+      <div
+        className={styles['lang-toggle']}
+        onClick={switchLanguage}
+        onMouseMove={e => mouseMoveInHeader(e)}
+        onMouseOut={mouseOutOfBlock}
+      >
         <div data-role='language-spin' data-lang={currentLanguage}>
           <h4 className='text-header text-bold'>ru</h4>
           <h4 className='text-header text-bold'>en</h4>
